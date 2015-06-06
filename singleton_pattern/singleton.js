615a7fcda87f844b4721959c99809c176ea7c37d
bruno yang <ygch1993@gmail.com>
2015-06-06 16:28:29 +0800

    add closure singleton

--- singleton_pattern/singleton.js
+++ singleton_pattern/singleton.js
@@ -1,18 +1,55 @@
 var Phone = {
   base: {
-    addressBook: function(){
+    addressBook: function() {
       return 'address book function';
     },
-    phone: function(){
+    phone: function() {
       return 'call someone';
     }
   },
   web: {
-    browser: function(){
+    browser: function() {
       return 'I can surfing in internet';
     }
   }
 };
+
+Car = (function() {
+  var arg = {
+    wheel: 4,
+    length: 5.1,
+    height: 2.2,
+    width: 3,
+    weight: 2
+  };
+
+  var broken = false;
+
+  function isBroken() {
+    if (arg.wheel < 4) {
+      broken = true;
+    }
+  }
+
+  isBroken();
+
+  return {
+    drive: function() {
+      if (!broken) {
+        console.log('drive');
+      }
+    },
+    transform: function() {
+      console.log('I am a transformer');
+    },
+    outputArg: function(name) {
+      console.log(arg[name]);
+    }
+  };
+})();
+
 var msg = document.getElementById('phone');
 
 msg.innerHTML = Phone.base.addressBook();
+
+var car = Car;
